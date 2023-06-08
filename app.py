62be57e1ac336cff65c7826e652e16e2a92e0a97
Saisi Shiribwa <119436503+lihlasaisi@users.noreply.github.com>
2023-06-08 10:06:19 +0300

    updated

--- app.py
+++ app.py
@@ -1,27 +1,63 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import uvicorn
+import pandas as pd
+import joblib
 
 app = FastAPI()
 
-@app.get("/predict-sepsis")
-def predict_sepsis(patientid: str, age: int, plasma_glucose: int, Blood_Pressure: int, Blood_Work_Result_1: int,  Blood_Work_Result_2: int, Blood_Work_Result_3: int, Blood_Work_Result_4: int, Body_mass_index: int, Insurance: str ):
-    # Load the random forest classifier
-    rf_model = RandomForestClassifier(max_depth=15, max_features=5, random_state=42)
 
-    # Load the patient's data
-    patient_data = {"patient id": patientid, "age": age, "plasma glucose": plasma_glucose, "Blood Pressure": Blood_Pressure, "Blood Work Result 1": Blood_Work_Result_1, "Blood Work Result 2": Blood_Work_Result_2, "Blood Work Result 3": Blood_Work_Result_3, "Blood Work Result 4": Blood_Work_Result_4, "Body mass index": Body_mass_index, "Insurance": Insurance}
+def load_model():
+    num_imputer = joblib.load('numerical_imputer.joblib')
+    scaler = joblib.load('scaler.joblib')
+    model = joblib.load('sepsis_model.joblib')
+    return num_imputer, scaler, model
 
-    # Convert the insurance status to a number
-    if Insurance == "Yes":
-        Insurance = 1
+
+def preprocess_input_data(sepsis_patient_data: dict, num_imputer, scaler):
+    input_df = pd.DataFrame(sepsis_patient_data, index=[0])
+    input_df = pd.DataFrame(num_imputer.transform(input_df), columns=input_df.columns)
+    input_df = pd.DataFrame(scaler.transform(input_df), columns=input_df.columns)
+    return input_df
+
+
+def predict_sepsis_from_data(sepsis_patient_data: pd.DataFrame, model):
+    prediction = model.predict(sepsis_patient_data)[0]
+    return prediction
+
+
+class SepsisPatientData(BaseModel):
+    age: int
+    plasma_glucose: int
+    blood_pressure: int
+    blood_work_result_1: int
+    blood_work_result_2: int
+    blood_work_result_3: int
+    blood_work_result_4: int
+    body_mass_index: int
+    insurance: int
+
+
+@app.get("/")
+def root():
+    return {"API": "Sepsis Prediction"}
+
+
+@app.post("/predict")
+def predict_sepsis(sepsis_patient_data: SepsisPatientData):
+    num_imputer, scaler, model = load_model()
+
+    input_df = preprocess_input_data(sepsis_patient_data.dict(), num_imputer, scaler)
+
+    prediction = predict_sepsis_from_data(input_df, model)
+
+    if prediction == 1:
+        result = "Positive Sepsis"
     else:
-        Insurance = 0
+        result = "Negative Sepsis"
 
-    # Predict whether or not the patient has sepsis
-    prediction = rf_model.predict(patient_data)
+    return {"result": result}
 
-    # Return the prediction
-    return {"prediction": prediction}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
